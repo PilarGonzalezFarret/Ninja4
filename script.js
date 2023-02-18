700d7b58ce9755e9a4778e9f2e002c5e3da50807
PilarGonzalezFarret <37448643+PilarGonzalezFarret@users.noreply.github.com>
2023-02-18 16:19:03 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+class Ninja {
+    constructor (nombre, salud, velocidad= 3, fuerza= 3){
+        this.nombre=nombre;
+        this.salud=salud;
+        this.velocidad= velocidad;
+        this.fuerza= fuerza;
+    }
+
+    sayName() {
+        console.log(this.nombre);
+    }
+
+    showStats() {
+        console.log(this.nombre, this.fuerza, this.velocidad, this.salud)
+    }
+
+    drinkSake() {
+        this.salud +=10;
+    }
+}
